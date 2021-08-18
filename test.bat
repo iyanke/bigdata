0a3ece3274f500f2eff4bab33ad44bf3b34b8179
Inna Yankelevich <inna_yan20@inbox.ru>
2021-08-18 16:47:41 +0300

    Hello 1test

--- test.bat
+++ test.bat
@@ -2,5 +2,5 @@ for /l %%x in (1, 1, 10) do  (
 echo "test%%x" >> "C:\!\git\bigdata\a6.txt"
 git add .  
 git commit -m "Hello %%xtest"
-git push https://iyanke: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@github.com/iyanke/bigdata --all 
+git push https://iyanke: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@github.com/iyanke/bigdata --all 
 )
