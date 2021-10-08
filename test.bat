8b3307325361d678126212173bb4641a05325880
Inna Yankelevich <inna_yan20@inbox.ru>
2021-10-08 17:03:36 +0300

    Hello 1test 123 456 789

--- test.bat
+++ test.bat
@@ -1,6 +1,6 @@
-for /l %%x in (1, 1, 10) do  (
+for /l %%x in (1, 1, 8) do  (
 echo "test%%x" >> "C:\!\git\bigdata\a6.txt"
 git add .  
-git commit -m "Hello %%xtest"
+git commit -m "Hello %%xtest 123 456 789"
 git push https://iyanke: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@github.com/iyanke/bigdata --all 
 )
